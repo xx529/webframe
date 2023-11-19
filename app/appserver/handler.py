e625bb5abdc9cc32225c78947a97d1ccab9fec44
Hang <64747715+xx529@users.noreply.github.com>
2023-11-19 14:46:26 +0800

    add handlers

--- app/appserver/handler.py
+++ app/appserver/handler.py
@@ -1,5 +1,8 @@
 from flask_restful import Resource
 from dataclasses import dataclass, asdict
+from functools import wraps
+import threading
+import uuid
 
 
 @dataclass
@@ -12,8 +15,56 @@ class ResponseJson:
         return asdict(self)
 
 
+class HandlerFuncs:
+
+    @staticmethod
+    def thread_handler(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            threading.current_thread().name = uuid.uuid4().hex
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    @staticmethod
+    def log_start_handler(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            threading.current_thread().name = uuid.uuid4().hex
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    @staticmethod
+    def auth_handler(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            print('auth')
+            x = func(*args, **kwargs)
+            print(x, 1)
+            return x
+
+        return wrapper
+
+    @staticmethod
+    def param_handler(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            print('param')
+            x = func(*args, **kwargs)
+            print(x, 2)
+            return x
+
+        return wrapper
+
+    @classmethod
+    def get_handler_funcs(cls):
+        return [cls.thread_handler, cls.log_start_handler, cls.auth_handler, cls.param_handler][::-1]
+
+
 class ServiceHandler(Resource):
     URL: str
+    method_decorators = HandlerFuncs.get_handler_funcs()
 
     @staticmethod
     def response(data, dtype='json', code=200):
@@ -25,7 +76,3 @@ class ServiceHandler(Resource):
                 return data, code, {'Content-Type': 'text/plain'}
             case _:
                 return data, code, {'Content-Type': 'application/json'}
-
-
-class RequestHandler:
-    pass
